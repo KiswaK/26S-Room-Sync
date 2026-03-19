5fc8b80d7d4b58af1c5c0694b10b0b0151cf6e1d
Mark Fontenot <m.fontenot@northeastern.edu>
2026-03-19 09:20:25 -0400

    Updated some of minor formatting and commenting issues

--- app/src/pages/12_API_Test.py
+++ app/src/pages/12_API_Test.py
@@ -1,5 +1,6 @@
 import logging
 logger = logging.getLogger(__name__)
+
 import streamlit as st
 import requests
 from modules.nav import SideBarLinks
@@ -9,18 +10,18 @@ st.set_page_config(layout='wide')
 SideBarLinks()
 
 st.write("# Accessing a REST API from Within Streamlit")
-"""
-Simply retrieving data from a REST api running in a separate Docker Container.
 
-If the container isn't running, this will be very unhappy.  But the Streamlit app 
-should not totally die. 
-"""
+st.write("""
+Simply retrieving data from a REST API running in a separate Docker container.
 
-data = {} 
+If the container isn't running, this page will fall back to dummy data.
+""")
+
+data = {}
 try:
-  data = requests.get('http://web-api:4000/data').json()
+    data = requests.get('http://web-api:4000/data').json()
 except requests.exceptions.RequestException as e:
-  st.write("**Important**: Could not connect to sample api, so using dummy data.")
-  data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
+    st.write("**Important**: Could not connect to sample API, so using dummy data.")
+    data = {"a": {"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
 
 st.dataframe(data)
